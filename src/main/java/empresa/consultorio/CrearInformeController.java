c531a3c96878bcc5c104613cdfe54b7174d0a6bd
Milagros Taboada <Milagros Taboada@DESKTOP-HOKLPCC>
2024-07-12 17:11:32 -0400

    Paciente, Alergia agregados 3

--- src/main/java/empresa/consultorio/CrearInformeController.java
+++ src/main/java/empresa/consultorio/CrearInformeController.java
@@ -1,26 +1,67 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package empresa.consultorio;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
-/**
- * FXML Controller class
- *
- * @author Milagros Taboada
- */
 public class CrearInformeController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button btnIzquierdo;
+    @FXML
+    private Button btnDerecho;
+    @FXML
+    private ImageView imIzquierdo;
+    @FXML
+    private ImageView imDerecho;
+
+    private final FileChooser fc = new FileChooser();
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        // Configurar directorio inicial y filtros de extensión
+        fc.setInitialDirectory(new File(System.getProperty("user.home")));
+        fc.getExtensionFilters().addAll(
+            new FileChooser.ExtensionFilter("Archivos de imagen", "*.png", "*.jpg", "*.gif")
+        );
+    }
+
+    @FXML
+    private void agregarOjoIzq(ActionEvent event) {
+        File file = fc.showOpenDialog(null);
+        if (file != null) {
+            try {
+                Image image = new Image(file.toURI().toURL().toExternalForm());
+                imIzquierdo.setImage(image);
+            } catch (Exception e) {
+                System.out.println("Error al cargar la imagen: " + e.getMessage());
+                e.printStackTrace();  // Imprime el rastreo de la pila para ver el origen del error
+            }
+        } else {
+            System.out.println("No se seleccionó ningún archivo");
+        }
+    }
+
+    @FXML
+    private void agregarOjoDer(ActionEvent event) {
+        File file = fc.showOpenDialog(null);
+        if (file != null) {
+            try {
+                Image image = new Image(file.toURI().toURL().toExternalForm());
+                imDerecho.setImage(image);
+            } catch (Exception e) {
+                System.out.println("Error al cargar la imagen: " + e.getMessage());
+                e.printStackTrace();  // Imprime el rastreo de la pila para ver el origen del error
+            }
+        } else {
+            System.out.println("No se seleccionó ningún archivo");
+        }
+    }
 }
